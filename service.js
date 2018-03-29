58bd9046b81b3cf7eb4da74da2004ceb713a67a4
tominop <2tominop@gmail.com>
2018-03-29 10:55:42 +0500

    update service

--- service.js
+++ service.js
@@ -54,7 +54,7 @@ app.options("/*", function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.status(200).send(200);
+    res.send(200);
 });
 
 //  Configuring express to use body-parser as middle-ware
